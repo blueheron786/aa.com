5207579d381d003cbcb8712de10a55a877f80a42
blueheron786 <blueheron786@gmail.com>
2025-04-28 19:03:07 -0400

    Ignore output for now; config changes.

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,16 @@
 AUTHOR = 'Ashiq Alibhai'
-SITENAME = 'AshiqAlibhai.com'
-SITEURL = ""
+SITENAME = 'Ashiq Alibhai, Author of Fantasy & Sci-Fi'
+SITESUBTITLE = 'Exploring Faith, Imagination, and Distant Worlds'
+SITEURL = "https://ashiqalibhai.com"
 
 PATH = "content"
 
 TIMEZONE = 'America/Toronto'
 
 DEFAULT_LANG = 'English'
+THEME = 'themes/flexing'
+
+STATIC_PATHS = ['images', 'pdfs', 'extra']
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -17,19 +21,13 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
 )
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
 )
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+RELATIVE_URLS = True
