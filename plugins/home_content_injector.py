ede642d812150810eb7b122d3395be887ef2ead3
blueheron786 <blueheron786@gmail.com>
2025-04-29 17:37:48 -0400

    Extract constant, comment code

--- plugins/home_content_injector.py
+++ plugins/home_content_injector.py
@@ -2,6 +2,8 @@ from pelican import signals
 import os
 import re
 
+NUM_BOOKS = 3
+
 def update_homepage_with_books(generator):
     content_path = generator.settings['PATH']
     home_md_path = os.path.join(content_path, 'pages', 'home.md')
@@ -9,7 +11,7 @@ def update_homepage_with_books(generator):
     # Find the latest 3 "Books" articles
     books = [article for article in generator.articles if article.category.name == "Books"]
     books.sort(key=lambda a: a.date, reverse=True)
-    latest_books = books[:3]
+    latest_books = books[:NUM_BOOKS]
 
     # Format the entries
     new_content = "\n".join([f"- [{a.title}]({a.url}) (published in {a.date.strftime('%B %Y')})" for a in latest_books])
@@ -18,6 +20,7 @@ def update_homepage_with_books(generator):
     with open(home_md_path, 'r', encoding='utf-8') as f:
         contents = f.read()
 
+    # Preserve start/end tags so we know what to replace on subsequent searches/replacements.
     updated = re.sub(
         r'<!-- start -->.*?<!-- end -->',
         f'<!-- start -->\n{new_content}\n<!-- end -->',
